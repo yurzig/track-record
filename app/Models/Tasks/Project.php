9d06ef78f3aca77b1a7d76dff869e2f6e2cc3afb
yurzi <ziganshin@bk.ru>
2025-04-10 21:18:20 +0300

    создан справочник projects 2

---
+++ app/Models/Tasks/Project.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models\Tasks;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Project extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = [
+        'parent_id',
+        'title',
+        'sort',
+    ];
+}
---
+++ database/migrations/2025_04_07_182625_create_projects_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('parent_id')->default(0);
+            $table->string('title');
+            $table->unsignedTinyInteger('sort')->default(0);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+    }
+};
